7c69e7ae0de12b5b594864bfd71d38bf4addfa2d
BeABraveDude <beabravedude@gmail.com>
2023-11-07 17:03:10 -0500

    Added custom theme. Removed boilerplate home page.
    Signed-off-by: BeABraveDude <beabravedude@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import React from "react";
+import type {Metadata} from 'next'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import ThemeRegistry from "@/theme/ThemeRegistry";
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body>
+    <ThemeRegistry options={{key: 'mui'}}>
+        {children}
+    </ThemeRegistry>
+    </body>
     </html>
   )
 }
