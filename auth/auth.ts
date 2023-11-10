54345c89935cdb0579abe0059625202b699db01e
BeABraveDude <beabravedude@gmail.com>
2023-11-09 19:29:35 -0500

    Added ability to check if controller is part of the facility.

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,10 +1,11 @@
-import {NextAuthOptions} from "next-auth";
+import {NextAuthOptions, User} from "next-auth";
 import {PrismaAdapter} from "@auth/prisma-adapter";
 import {PrismaClient} from "@prisma/client";
 import VatsimProvider from "@/auth/vatsimProvider";
 import {Adapter} from "next-auth/adapters";
 
 const prisma = new PrismaClient();
+const VATUSA_FACILITY = process.env['VATUSA_FACILITY'];
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
@@ -15,6 +16,22 @@ export const authOptions: NextAuthOptions = {
         ),
     ],
     callbacks: {
+        signIn: async ({user}) => {
+
+            const rosterCheckDisabled: boolean = process.env['DISABLE_ROSTER_CHECK'] === 'true';
+
+            if (rosterCheckDisabled) {
+                return true;
+            } else {
+                const res = await fetch(`https://api.vatusa.net/v2/facility/${VATUSA_FACILITY}/roster/both`);
+                const data = await res.json();
+                const controllers = data.data as User[];
+                const controllers_cid = controllers.map((controller) => controller.cid);
+
+                return controllers_cid.includes(user.cid);
+            }
+
+        },
         session: async ({session, user}) => {
             session.user = user;
             return session;
