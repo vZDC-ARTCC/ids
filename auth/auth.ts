7528d7068d88d976da88859c3f4f192e3f640d26
BeABraveDude <beabravedude@gmail.com>
2023-11-09 20:24:36 -0500

    Added comments to make code more readable.

--- auth/auth.ts
+++ auth/auth.ts
@@ -4,36 +4,57 @@ import {PrismaClient} from "@prisma/client";
 import VatsimProvider from "@/auth/vatsimProvider";
 import {Adapter} from "next-auth/adapters";
 
+// grab a new instance of Prisma
 const prisma = new PrismaClient();
+
+// get facility from environment variables
 const VATUSA_FACILITY = process.env['VATUSA_FACILITY'];
 
 export const authOptions: NextAuthOptions = {
+    // configure next-auth to use prisma
     adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
+        // define VATSIM as one of our oauth providers
         VatsimProvider(
             process.env['VATSIM_CLIENT_ID'],
             process.env['VATSIM_CLIENT_SECRET'],
         ),
     ],
     callbacks: {
+        // method to check if user is allowed to sign in.
+        // using this to check if controller is part of the facility defined in environment variables
         signIn: async ({user}) => {
 
+            // convert the DISABLE_ROSTER_CHECK environment variable to a boolean
             const rosterCheckDisabled: boolean = process.env['DISABLE_ROSTER_CHECK'] === 'true';
 
+            // if roster check is disabled, any controller is able to sign in and the facility check is skipped
             if (rosterCheckDisabled) {
                 return true;
             } else {
+                // fetch roster for facility defined in environment variables
                 const res = await fetch(`https://api.vatusa.net/v2/facility/${VATUSA_FACILITY}/roster/both`);
                 const data = await res.json();
+                // { data: [{ cid: number,... },...], }
+                // extract the array from 'data' and tell typescript that there is guaranteed to be a cid in all the values
                 const controllers = data.data as User[];
+                // remove all the other attributes from the list to leave a list of ONLY cids
+                // [cid....]
                 const controllers_cid = controllers.map((controller) => controller.cid);
 
+                // return true of false depending on if the user is part of the list of cids, hence being part of the roster
+                // if the user is not allowed to sign in, a page is displayed notifying them.
                 return controllers_cid.includes(user.cid);
             }
 
         },
+        // method that executes everytime the session is retrieved
         session: async ({session, user}) => {
+            // add the user to the session so more than just the email is visible to the client
+            // cid, rating, and all other fields are not visible
             session.user = user;
+
+            // return the updated session
             return session;
         }
     }
--- auth/vatsimProvider.ts
+++ auth/vatsimProvider.ts
@@ -1,23 +1,34 @@
 import {OAuthConfig} from "@auth/core/providers";
 import {Profile, User} from "next-auth";
 
+// vatsim oauth endpoint base url from environment variables
 const VATSIM_URL = process.env['VATSIM_OAUTH_ENDPOINT'];
 
+// enables authentication with vatsim
+// needs a client and secret key to work correctly
+// https://www.vatsim.dev
 export default function VatsimProvider(clientId?: string, clientSecret?: string) {
     return {
         id: 'vatsim',
         name: 'VATSIM',
         type: 'oauth',
+        // when a user attempts to sign in, a link is generated with the following scopes.
+        // the url property is just the base url for all the other parameters to attach to
         authorization: {
             url: `${VATSIM_URL}/oauth/authorize`,
             params: {scope: "email vatsim_details full_name"},
         },
+        // after a user has logged in, a code is passed back to this app from VATSIM.
+        // using this code, an access token is fetched using the following url
+        // again, parameters are automatically added thanks to next-auth
         token: {
             url: `${VATSIM_URL}/oauth/token`,
         },
+        // once the access token is obtained, it is used to fetched data about the user, such as cid, rating, name, etc..
         userinfo: {
             url: `${VATSIM_URL}/api/user`,
         },
+        // user is transformed into a different format than what VATSIM provides before being saved in the database and logged in
         profile: ({data}: { data: Profile }) => {
             return {
                 id: data.cid,
