1ff640fa96d0f603fdaf016dab4b81309c07eeb8
Vainnor <142100426+Vainnor@users.noreply.github.com>
2023-11-23 15:04:03 -0500

    added rdu to facilities.ts

--- facility/facilities.ts
+++ facility/facilities.ts
@@ -1,8 +1,10 @@
 import {PCT_CONFIG} from "@/facility/tracon/pct/pct";
 import {TraconConfig} from "@/types";
+import {RDU_CONFIG} from "@/facility/tracon/rdu/rdu-tracon";
 
 export const IDS_TRACON_FACILITIES: TraconConfig[] = [
     PCT_CONFIG,
+    RDU_CONFIG,
 ];
 
 
