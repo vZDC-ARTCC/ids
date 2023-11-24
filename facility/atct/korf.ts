d12536f0c5a68a818d806fc5fa1b86254102f032
Vainnor <142100426+Vainnor@users.noreply.github.com>
2023-11-24 14:15:31 -0500

    Added KORF ATCT and TRACON.
    Departure gates have not been added

---
+++ facility/atct/korf.ts
@@ -0,0 +1,120 @@
+import {AirportConfig} from "@/types";
+
+export const orfAirportConfig: AirportConfig = {
+    faaIdentifier: 'ORF',
+    icao: 'KORF',
+    parentTracon: 'ORF',
+    parentTraconArea: 'ORF',
+    availableRunways: [
+        {
+            id: '05',
+            availableApproachTypes: ['RIV-VIS', 'ILS', 'GPS-Z','RNP-Y'],
+            availableDepartureTypes: ['RV'],
+        },
+        {
+            id: '23',
+            availableApproachTypes: ['VIS', 'ILS', 'GPS-Z','RNP-Y'],
+            availableDepartureTypes: ['RV'],
+        },
+        {
+            id: '14',
+            availableApproachTypes: ['VIS', 'GPS','VOR'],
+            availableDepartureTypes: ['RV'],
+        },
+        {
+            id: '32',
+            availableApproachTypes: ['VIS', 'GPS','VOR'],
+            availableDepartureTypes: ['RV'],
+        },
+    ],
+    flows: [
+        {
+            name: 'WEST VMC',
+            departureRunways: [
+                {
+                    id: '23',
+                    availableDepartureTypes: ['RV'],
+                },
+            ],
+            arrivalRunways: [
+                {
+                    id: '23',
+                    availableApproachTypes: ['VIS', 'ILS', 'GPS-Z','RNP-Y'],
+                },
+            ],
+            traconVisibleOptions: [
+                {
+                    name: '14/32',
+                    choices: ['OPEN', 'CLOSED'],
+                },
+            ],
+        },
+        {
+            name: 'WEST IMC',
+            departureRunways: [
+                {
+                    id: '23',
+                    availableDepartureTypes: ['RV'],
+                },
+            ],
+            arrivalRunways: [
+                {
+                    id: '23',
+                    availableApproachTypes: ['ILS', 'GPS-Z','RNP-Y'],
+                },
+            ],
+            traconVisibleOptions: [
+                {
+                    name: '14/32',
+                    choices: ['OPEN', 'CLOSED'],
+                },
+            ],
+        },
+        {
+            name: 'EAST VMC',
+            departureRunways: [
+                {
+                    id: '05',
+                    availableDepartureTypes: ['RV'],
+                },
+            ],
+            arrivalRunways: [
+                {
+                    id: '05',
+                    availableApproachTypes: ['RIV-VIS', 'ILS', 'GPS-Z','RNP-Y'],
+                },
+            ],
+            traconVisibleOptions: [
+                {
+                    name: '14/32',
+                    choices: ['OPEN', 'CLOSED'],
+                },
+            ],
+        },
+        {
+            name: 'EAST IMC',
+            departureRunways: [
+                {
+                    id: '05',
+                    availableDepartureTypes: ['RV'],
+                },
+            ],
+            arrivalRunways: [
+                {
+                    id: '05',
+                    availableApproachTypes: ['ILS', 'GPS-Z','RNP-Y'],
+                },
+            ],
+            traconVisibleOptions: [
+                {
+                    name: '14/32',
+                    choices: ['OPEN', 'CLOSED'],
+                },
+            ],
+        },
+    ],
+    localControlPositions: ['LC'],
+    customizableOptions: [],
+    sop: '/sop/orf_sop.pdf',
+    sidebarOptions: [],
+}
--- facility/facilities.ts
+++ facility/facilities.ts
@@ -1,10 +1,12 @@
 import {PCT_CONFIG} from "@/facility/tracon/pct/pct";
 import {TraconConfig} from "@/types";
 import {RDU_CONFIG} from "@/facility/tracon/rdu/rdu-tracon";
+import {ORF_CONFIG} from "@/facility/tracon/orf/orf-tracon";
 
 export const IDS_TRACON_FACILITIES: TraconConfig[] = [
     PCT_CONFIG,
     RDU_CONFIG,
+    ORF_CONFIG,
 ];
 
 
