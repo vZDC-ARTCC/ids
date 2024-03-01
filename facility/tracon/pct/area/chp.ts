2b3076c6b805ccee40dce58c3d5c0859c28be68b
Carson Berget <142100426+Vainnor@users.noreply.github.com>
2024-02-29 20:19:08 -0500

    fixed chp area file to include kesn

--- facility/tracon/pct/area/chp.ts
+++ facility/tracon/pct/area/chp.ts
@@ -2,13 +2,14 @@ import {TraconAreaConfig} from "@/types";
 
 import {bwiAirportConfig} from "@/facility/atct/kbwi";
 import {mtnAirportConfig} from "@/facility/atct/kmtn";
+import {esnAirportConfig} from "@/facility/atct/kesn";
 
 export const PCT_CHP: TraconAreaConfig = {
     faaIdentifier: 'CHP',
     name: 'Chesapeake',
     sop: '/sop/pct_chp_sop.pdf',
     majorFields: [bwiAirportConfig],
-    minorFields: [mtnAirportConfig],
+    minorFields: [mtnAirportConfig, esnAirportConfig],
     customizableOptions: [],
     areaMaps: [
         {
